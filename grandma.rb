48abd2c9fa20fd37e1b246f89d75214da1f5c9af
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-05 17:56:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma (string)
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
   if string == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
 
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
