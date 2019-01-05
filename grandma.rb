e136a6d63661deffaf9251125e7dc7c50e2bde64
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-05 17:51:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,10 +7,11 @@ def speak_to_grandma (string)
 if string != string.upcase
   puts "HUH?! SPEAK UP, SONNY!"
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
-
 # NO, NOT SINCE 1938!
+else string == string.upcase
+  puts "NO, NOT SINCE 1938!"
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
